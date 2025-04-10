eb5302709a620bcc8c351ed63527b71bd71e9b53
PedroMessias101 <pedromessias101@gmail.com>
2025-04-10 10:17:54 -0300

    Adicionando na tela

--- Front-09/script.js
+++ Front-09/script.js
@@ -1,29 +1,61 @@
 const cars = [
-    {
-        brand: "toyota",
-        model: "corola",
-        year: 2020
-    },
+  {
+    brand: "Toyota",
+    model: "Corola",
+    year: 2020,
+  },
 
-    {
-        brand: "Honda",
-        model: "Civic",
-        year: 2019
-    },
-    {
-        brand: "Ford",
-        model: "Fiesta",
-        year: 2014
-    },
-    {
-        brand: "Ford",
-        model: "Focus",
-        year: 2018
-    },
+  {
+    brand: "Honda",
+    model: "Civic",
+    year: 2019,
+  },
+  {
+    brand: "Ford",
+    model: "Fiesta",
+    year: 2014,
+  },
+  {
+    brand: "Ford",
+    model: "Focus",
+    year: 2018,
+  },
+  {
+    brand: "Fiat",
+    model: "Uno",
+    year: 2013,
+  },
 ];
 
+/* criando uma função */
+function displayCars() {
+  // variavel
+  const cardList = document.querySelector("#card-list");
+  
 
-console.log(cars[0].brand)
-console.log(cars[2].model)
+  cars.forEach((car) => {
+    console.log(car);
+    // Criando um elemento div pelo JavaScript
+    const cardDiv = document.createElement("div")
+
+    // criando uma classe css car
+    cardDiv.classList.add("car");
+
+// Criando o modelo
+    const cardModel = document.createElement("h2")
+    cardModel.textContent = `${car.brand} ${car.model}`;
+
+    // Criando o ano
+    const cardYear = document.createElement("p")
+    cardYear.textContent = `Ano: ${car.year}`
+
+    cardDiv.appendChild(cardModel);
+    cardDiv.appendChild(cardYear)
 
 
+    cardList.appendChild(cardDiv)
+  });
+}
+
+// Chamar função
+displayCars();
